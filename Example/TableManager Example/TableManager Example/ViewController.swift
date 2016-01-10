b72bd2ddbb21c5aa3c485734f13990c28a20e010
Henrique Morbin <morbin_@hotmail.com>
2016-01-10 14:21:43 -0200

    Example code

--- Example/TableManager Example/TableManager Example/ViewController.swift
+++ Example/TableManager Example/TableManager Example/ViewController.swift
@@ -17,12 +17,56 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        configTableManager()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Methods
+    
+    func configTableManager(){
+        
+        //1 - Adding a section
+        tableManager.sections.append(Section())
+        
+        //2 - Data to fill table
+        let data = ["Basic Usage", "Row Selection", "Sections & Rows Visibility", "Custom Cells"]
+        
+        for value in data {
+            
+            //3 - Creating a row
+            let row = Row(identifier: "CellBasic", object: value) { (object, cell, indexPath) -> Void in
+                if let object = object as? String {
+                    cell.textLabel?.text = object
+                }
+            }
+            
+            //4 - Adding selection handler
+            row.didSelectRowAtIndexPath = { (row: Row, tableView: UITableView, indexPath: NSIndexPath) -> Void in
+                if let object = row.object as? String {
+                    self.showAlert(object)
+                }
+            }
+            
+            //5 - Adding this row in table
+            tableManager.sections[0].rows.append(row)
+        }
+        
+        //6 - Refreshing table UI
+        tableManager.reloadData()
+    }
+    
+    func showAlert(message : String){
+        
+        let alert = UIAlertController(title: "Message", message: message, preferredStyle: .Alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+        
+        presentViewController(alert, animated: true, completion: nil)
     }
 }
 
