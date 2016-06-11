448fe940b471302ffb4c923012f9a76a71809edf
Henrique Morbin <morbin_@hotmail.com>
2016-06-11 16:28:06 -0300

    Fix podspec

--- Pod/Classes/TableViewExtension.swift
+++ Pod/Classes/TableViewExtension.swift
@@ -10,7 +10,7 @@ import UIKit
 
 internal let instanceKey = "tableManagerInstance"
 
-extension UITableView {
+public extension UITableView {
     
     public func tableManagerInstance() -> TableManager {
         guard let tableManager = self.layer.valueForKey(instanceKey) as? TableManager else {
