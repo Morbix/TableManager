6e8748571b289af2a0d7102f766a10ead0e6c5e3
Cleber <cleberhenriques@gmail.com>
2016-06-15 23:25:04 -0300

    Supporting carthage

---
+++ TableManager/Carthage/Build/iOS/TableManager.framework/Headers/TableManager.h
@@ -0,0 +1,19 @@
+//
+//  TableManager.h
+//  TableManager
+//
+//  Created by Cleber Henriques on 6/15/16.
+//  Copyright © 2016 Morbix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TableManager.
+FOUNDATION_EXPORT double TableManagerVersionNumber;
+
+//! Project version string for TableManager.
+FOUNDATION_EXPORT const unsigned char TableManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TableManager/PublicHeader.h>
+
+
