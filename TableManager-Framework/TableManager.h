861e69ae7434e4ba05f76b5d27797e120ec452f6
Cleber <cleberhenriques@gmail.com>
2016-06-16 23:35:15 -0300

    Re-creating framework project, organizing folders

---
+++ TableManager-Framework/TableManager.h
@@ -0,0 +1,19 @@
+//
+//  TableManager-Framework.h
+//  TableManager-Framework
+//
+//  Created by Cleber Henriques on 6/16/16.
+//  Copyright © 2016 morbix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TableManager-Framework.
+FOUNDATION_EXPORT double TableManager_FrameworkVersionNumber;
+
+//! Project version string for TableManager-Framework.
+FOUNDATION_EXPORT const unsigned char TableManager_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TableManager_Framework/PublicHeader.h>
+
+
