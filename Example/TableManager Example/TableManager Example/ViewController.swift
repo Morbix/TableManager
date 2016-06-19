f4481e724beb97e6f7a1e330bea185e1ecbfe295
Henrique Morbin <morbin_@hotmail.com>
2016-06-19 13:44:53 -0300

    Home refactor

--- Example/TableManager Example/TableManager Example/ViewController.swift
+++ Example/TableManager Example/TableManager Example/ViewController.swift
@@ -16,103 +16,65 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        getExamples().forEach { (title, didSelect) in
+        let examples = [
+            Screen.BasicUsage,
+            Screen.Visibility,
+            Screen.CustomCell,
+            Screen.Selection,
+            Screen.DragAndDrop,
+            Screen.Deletion
+        ]
+        
+        examples.forEach { screen in
             
             let row = tableView.addRow("CellBasic")
             
             row.setConfiguration { (row, cell, indexPath) in
-                cell.textLabel?.text = title
-                
-                if let _ = row.didSelect {
-                    cell.textLabel?.font = UIFont.systemFontOfSize(16)
-                    cell.textLabel?.textAlignment = .Left
-                    cell.accessoryType = .DisclosureIndicator
-                    cell.selectionStyle = .Default
-                } else {
-                    cell.textLabel?.font = UIFont.boldSystemFontOfSize(16)
-                    cell.textLabel?.textAlignment = .Center
-                    cell.accessoryType = .None
-                    cell.selectionStyle = .None
-                }
+                cell.textLabel?.text = screen.rawValue
             }
             
-            row.didSelect = didSelect
+            row.didSelect = self.navigateTo(screen)
         }
         
         tableView.reloadData()
     }
     
-    // MARK: Methodos
-    
-    final private func getExamples() -> [(String, Row.DidSelect?)] {
-        return [
-            ("Basic Usage", navigateToBasicUsage()),
-            ("Complex Usage", navigateToComplexUsage()),
-            ("Documentation", nil),
-            ("Visibility", navigateToVisibility()),
-            ("Custom Cell", navigateToCustomCell()),
-            ("Selection", navigateToSelection()),
-            ("Drag & Drop", navigateToDragAndDrop()),
-            ("Deletion", navigateToDeletion())
-        ]
-    }
-
-    final private func presentNotAvailableMessage() {
-        let alert = UIAlertController(title: nil, message: "Not available yet.", preferredStyle: .Alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
-        self.presentViewController(alert, animated: true, completion: nil)
-    }
-    
     // MARK: Actions
     
-    final private func navigateToBasicUsage() -> Row.DidSelect {
+    final private func navigateTo(screen: Screen) -> Row.DidSelect {
         return { row, tableView, indexPath in
             tableView.deselectRowAtIndexPath(indexPath, animated: true)
-            self.navigationController?.pushViewController(BasicUsageViewController(), animated: true)
+            self.navigationController?.pushViewController(screen.getViewController(), animated: true)
         }
     }
+
+}
+
+private enum Screen: String {
+    case BasicUsage = "Basic Usage"
+    case ComplexUsage = "Complex Usage"
+    case Visibility = "Visibility"
+    case CustomCell = "Custom Cell"
+    case Selection = "Selection"
+    case DragAndDrop = "Drag & Drop"
+    case Deletion = "Deletion"
     
-    final private func navigateToComplexUsage() -> Row.DidSelect {
-        return { row, tableView, indexPath in
-            tableView.deselectRowAtIndexPath(indexPath, animated: true)
-            
-            self.presentNotAvailableMessage()
+    func getViewController() -> UIViewController {
+        switch self {
+        case .BasicUsage:
+            return BasicUsageViewController()
+        case .ComplexUsage:
+            return UIViewController()
+        case .Visibility:
+            return VisibilityViewController()
+        case .CustomCell:
+            return CustomCellViewController()
+        case .Selection:
+            return SelectionViewController()
+        case .DragAndDrop:
+            return DragAndDropViewController()
+        case .Deletion:
+            return DeletionViewController()
         }
     }
-    
-    final private func navigateToVisibility() -> Row.DidSelect {
-        return { row, tableView, indexPath in
-            tableView.deselectRowAtIndexPath(indexPath, animated: true)
-            self.navigationController?.pushViewController(VisibilityViewController(), animated: true)
-        }
-    }
-    
-    final private func navigateToCustomCell() -> Row.DidSelect {
-        return { row, tableView, indexPath in
-            tableView.deselectRowAtIndexPath(indexPath, animated: true)
-            self.navigationController?.pushViewController(CustomCellViewController(), animated: true)
-        }
-    }
-    
-    final private func navigateToSelection() -> Row.DidSelect {
-        return { row, tableView, indexPath in
-            tableView.deselectRowAtIndexPath(indexPath, animated: true)
-            self.navigationController?.pushViewController(SelectionViewController(), animated: true)
-        }
-    }
-    
-    final private func navigateToDragAndDrop() -> Row.DidSelect {
-        return { row, tableView, indexPath in
-            tableView.deselectRowAtIndexPath(indexPath, animated: true)
-            self.navigationController?.pushViewController(DragAndDropViewController(), animated: true)
-        }
-    }
-    
-    final private func navigateToDeletion() -> Row.DidSelect {
-        return { row, tableView, indexPath in
-            tableView.deselectRowAtIndexPath(indexPath, animated: true)
-            self.navigationController?.pushViewController(DeletionViewController(), animated: true)
-        }
-    }
-    
 }
